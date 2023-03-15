70b8a2cb98d3acc6192336d3023ff6f077f0de87
roxyzc <roxyzc12@gmail.com>
2023-03-15 18:58:23 +0700

    update

--- src/controllers/products/post.controller.ts
+++ src/controllers/products/post.controller.ts
@@ -4,12 +4,27 @@ import Product from "models/product.model";
 const postProduct = async (req: Request, res: Response, next: NextFunction) => {
   const { namaProduct, harga, stok } = req.body;
   try {
-    const findProduct = await Product.create({
-      namaProduct,
-      harga,
-      stok,
-      image: "https://cdn.discordapp.com/attachments/755224936414445629/1085156061255831582/no-image-icon-10.png",
+    const findProduct = await Product.findOne({
+      where: {
+        namaProduct,
+      },
+    }).then(async (value) => {
+      if (value === null) {
+        const product = await Product.create({
+          namaProduct,
+          harga,
+          stok,
+          image: "https://cdn.discordapp.com/attachments/755224936414445629/1085156061255831582/no-image-icon-10.png",
+        });
+        return product;
+      }
+      await value.update({
+        stok: value.getDataValue("stok") + Number(stok),
+      });
+      await value.reload();
+      return value;
     });
+
     res.status(200).json({ success: true, data: { Product: findProduct } });
   } catch (error) {
     next(error);
